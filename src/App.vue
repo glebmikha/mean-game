12a8d9771ebe9bcec655d998343d2002374dc75f
Gleb Mikhaylov <gm.mikhailov@gmail.com>
2019-06-21 14:59:23 +0300

    added footer

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,18 @@
     <v-content>
       <router-view/>
     </v-content>
+    <app-footer/>
   </v-app>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
 export default {
   name: "App",
   components: {
-    "app-navbar": Navbar
+    "app-navbar": Navbar,
+    "app-footer": Footer
   },
   data() {
     return {
---
+++ src/components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-footer dark height="auto">
+    <v-card class="flex" flat tile>
+      <!-- <v-card-title class="teal">
+        <strong class="subheading">Get connected with me on social networks!</strong>
+
+        <v-spacer></v-spacer>
+
+        <v-btn v-for="icon in icons" :key="icon" class="mx-3" dark icon>
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-title>-->
+
+      <v-card-actions class="grey darken-3 justify-center">
+        &copy;2019 —
+        <a href="https://github.com/glebmikha" target="_blank">
+          <strong>Gleb Mikhaylov</strong>
+        </a>
+      </v-card-actions>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    icons: [
+      "fab fa-facebook",
+      "fab fa-twitter",
+      "fab fa-google-plus",
+      "fab fa-linkedin",
+      "fab fa-instagram"
+    ]
+  })
+};
+</script>
