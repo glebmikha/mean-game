3e80275282060558cc08a548839db88c709d0648
Gleb Mikhaylov <gm.mikhailov@gmail.com>
2019-06-21 12:42:35 +0300

    added navigation drawer for small screens

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,35 @@
 <template>
-  <v-toolbar :dark="true">
-    <!-- <v-toolbar-side-icon></v-toolbar-side-icon> -->
-    <v-toolbar-title>Mean Game</v-toolbar-title>
-    <v-spacer></v-spacer>
-    <v-toolbar-items>
-      <v-btn flat to="/">Play</v-btn>
-
-      <v-btn v-if="!user" flat to="/signin">Sign In</v-btn>
-      <v-btn v-if="!user" flat to="/signup">Sign Up</v-btn>
-      <v-btn v-if="user" flat @click="signout">Sign Out</v-btn>
-    </v-toolbar-items>
-  </v-toolbar>
+  <nav>
+    <v-navigation-drawer absolute temporary v-model="drawer" class="hidden-md-and-up">
+      <v-list>
+        <v-list-tile>
+          <v-btn flat to="/">Play</v-btn>
+        </v-list-tile>
+        <v-list-tile>
+          <v-btn v-if="!user" flat to="/signin">Sign In</v-btn>
+        </v-list-tile>
+        <v-list-tile>
+          <v-btn v-if="!user" flat to="/signup">Sign Up</v-btn>
+        </v-list-tile>
+        <v-list-tile>
+          <v-btn v-if="user" flat @click="signout">Sign Out</v-btn>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar :dark="true">
+      <v-toolbar-side-icon @click.stop="drawer = !drawer" class="hidden-md-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" style="cursor: pointer">Mean Game</router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat to="/">Play</v-btn>
+        <v-btn v-if="!user" flat to="/signin">Sign In</v-btn>
+        <v-btn v-if="!user" flat to="/signup">Sign Up</v-btn>
+        <v-btn v-if="user" flat @click="signout">Sign Out</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </nav>
 </template>
 
 <script>
@@ -20,7 +39,8 @@ export default {
   name: "Navbar",
   data() {
     return {
-      user: null
+      user: null,
+      drawer: false
     };
   },
   created() {
@@ -35,7 +55,7 @@ export default {
   methods: {
     signout() {
       auth.signOut().then(() => {
-        this.$router.push({ name: "signin" });
+        this.$router.push({ name: "home" });
       });
     }
   }
