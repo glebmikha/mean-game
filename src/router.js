cc86ff703bd2dd5aaeb4bff7c457b89f5eaef15a
Gleb Mikhaylov <gm.mikhailov@gmail.com>
2019-06-23 15:57:15 +0300

    added user profile

--- src/router.js
+++ src/router.js
@@ -3,10 +3,12 @@ import Router from "vue-router";
 import Home from "./views/Home.vue";
 import Signin from "./views/Signin.vue";
 import Signup from "./views/Signup.vue";
+import Profile from "./views/Profile.vue";
+import { auth } from "@/firebase/init";
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
@@ -24,6 +26,37 @@ export default new Router({
       path: "/signup",
       name: "signup",
       component: Signup
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      component: Profile,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 });
+
+// router guards
+router.beforeEach((to, from, next) => {
+  // check to see if route has auth guard
+  if (to.matched.some(rec => rec.meta.requiresAuth)) {
+    // check auth state of user
+    let user = auth.currentUser;
+    if (user) {
+      // User is signed in. Proceed to route
+      next();
+    } else {
+      // No user is signed in. Redirect to login
+      next({
+        name: "signin"
+      });
+    }
+  } else {
+    // if route is not guarded by auth, proceed
+    next();
+  }
+});
+
+export default router;
---
+++ src/views/Profile.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-content>
+    <v-container fluid fill-height>
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md4>
+          <v-card class="elevation-12">
+            <v-toolbar :dark="true">
+              <v-toolbar-title>Profile</v-toolbar-title>
+              <v-spacer></v-spacer>
+            </v-toolbar>
+            <v-card-text>
+              <v-form @submit.prevent="signup" id="signup-form">
+                <v-text-field
+                  prepend-icon="email"
+                  name="email"
+                  label="Email"
+                  type="text"
+                  v-model="email"
+                  disabled
+                ></v-text-field>
+                <v-text-field
+                  prepend-icon="person"
+                  name="name"
+                  label="Name"
+                  type="text"
+                  v-model="name"
+                  disabled
+                ></v-text-field>
+                <v-flex xs12 md8 offset-md2>
+                  <v-card-text>
+                    <h3>Your average error is</h3>
+                  </v-card-text>
+                </v-flex>
+              </v-form>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
+</template>
+<script>
+import { auth, db } from "@/firebase/init";
+export default {
+  data() {
+    return {
+      email: null,
+      name: null
+    };
+  },
+  created() {
+    let uid = auth.currentUser.uid;
+    this.email = auth.currentUser.email;
+    db.collection("users")
+      .where("user_id", "==", uid)
+      .get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          this.name = doc.data().name;
+        });
+      });
+  }
+};
+</script>
+
+<style>
+</style>
